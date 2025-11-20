1e5fa5379898c68d79f43fa48913f5b72e2d3a1f
Eduardo <eduardo.raffi94@gmail.com>
2025-11-20 11:44:06 -0300

    Initial commit

---
+++ PopularSwiftRepositories/PopularSwiftRepositories/PopularSwiftRepositoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopularSwiftRepositoriesApp.swift
+//  PopularSwiftRepositories
+//
+//  Created by Eduardo Raffi on 20/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PopularSwiftRepositoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PopularSwiftRepositories/PopularSwiftRepositoriesTests/PopularSwiftRepositoriesTests.swift
@@ -0,0 +1,17 @@
+//
+//  PopularSwiftRepositoriesTests.swift
+//  PopularSwiftRepositoriesTests
+//
+//  Created by Eduardo Raffi on 20/11/25.
+//
+
+import Testing
+@testable import PopularSwiftRepositories
+
+struct PopularSwiftRepositoriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
